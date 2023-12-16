582b25ddacdff160611b606d019c355b79aa2fa6
SK-Yeboah <skyeboah9@gmail.com>
2023-12-16 17:15:28 +0000

    shell sort opt

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -21,7 +21,7 @@ void swap(int *a, int *b)
  * @size: Number of elements in the array
  */
 
-/*void shell_sort(int *array, size_t size)
+void shell_sort(int *array, size_t size)
 {
 
     size_t gap;
@@ -56,34 +56,5 @@ void swap(int *a, int *b)
             }
         }
     }
-}*/
-
-
-/**
- * shell_sort - Sorts an array of integers in ascending order using
- *              the Shell sort algorithm with the Knuth sequence
- * @array: The array to be sorted
- * @size: Number of elements in the array
- */
-void shell_sort(int *array, size_t size)
-{
-
-    size_t gap, i, j;
-
-    if (array == NULL || size <= 1)
-        return;
-
-    
-
-    for (gap = size / 2; gap > 0; gap /= 2)
-    {
-        for (j = gap; j < size; ++j)
-        {
-            for (i = j; i >= gap && array[i - gap] > array[i]; i -= gap)
-            {
-                swap(&array[i], &array[i - gap]);
-                print_array(array, size);
-            }
-        }
-    }
 }
+
