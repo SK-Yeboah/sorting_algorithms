311ba1d74b0a619093f4a40892790599dd61722a
SK-Yeboah <skyeboah9@gmail.com>
2023-12-16 11:27:50 +0000

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i;
+    size_t j;
+
+    if(array == NULL || size < 2)
+    {
+        return;
+    }
+
+    for ( i = 0; i < size -1; i++)
+    {
+            for(j = 0; j < size - i -1; j++)
+            {
+                if(array[j] > array[j+1])
+                {
+                    swap(&array[j], &array[j+1]);
+
+                    print_array(array, size);
+                }
+            }
+    }
+}
