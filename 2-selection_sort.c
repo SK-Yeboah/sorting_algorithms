7b82b4ac11c358e69e0a10869e9dab9dfa4da8e3
SK-Yeboah <skyeboah9@gmail.com>
2023-12-21 22:07:54 +0000

    insertion_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,10 @@
 #include "sort.h"
 
+/**
+ * swap - Swaps two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
 void swap(int *a, int *b)
 {
     int temp = *a;
