bd4da557c219dfc0b3ad8476a1dbb3d1cc490c43
SK-Yeboah <skyeboah9@gmail.com>
2023-12-21 22:17:25 +0000

    insertion_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,7 +19,7 @@ void swap(int *a, int *b)
  * @array: The array to be sorted
  * @size: Number of elements in the array
  */
-/*void selection_sort(int *array, size_t size)
+void selection_sort(int *array, size_t size)
 {
     size_t i;
     size_t j;
@@ -42,7 +42,8 @@ void swap(int *a, int *b)
         {
             swap(&array[i], &array[min_index]);
 
-           
+            /* Print the array after each swap */
+            /*printf("[Swap %lu]: ", i + 1);*/
             for (k = 0; k < size; ++k)
             {
                 if (k > 0)
@@ -52,49 +53,6 @@ void swap(int *a, int *b)
             printf("\n");
         }
     }
-}*/
-
-
-/**
- * selection_sort - Sorts an array of integers in ascending order using
- *                  the Selection sort algorithm
- * @array: The array to be sorted
- * @size: Number of elements in the array
- * @print_flag: Flag to indicate whether to print the array after each swap
- */
-void selection_sort(int *array, size_t size, int print_flag)
-{
-    if (array == NULL || size <= 1)
-        return;
-
-    size_t i, j, k;
-
-    for (i = 0; i < size - 1; ++i)
-    {
-        size_t min_index = i;
-
-        for (j = i + 1; j < size; ++j)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
-
-        if (min_index != i)
-        {
-            swap(&array[i], &array[min_index]);
-
-            /* Print the array after each swap if the flag is enabled */
-            if (print_flag)
-            {
-                printf("[Swap %lu]: ", i + 1);
-                for (k = 0; k < size; ++k)
-                {
-                    if (k > 0)
-                        printf(", ");
-                    printf("%d", array[k]);
-                }
-                printf("\n");
-            }
-        }
-    }
 }
+
+
